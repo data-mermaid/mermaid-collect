bbc4e79ea0ca3c38f9d978edec7243b111e0ecb0
Dustin Sampson <gridcell@gmail.com>
2019-09-11 13:17:31 -0700

    simplify nav item highlight and menu collapse

--- src/app/_mermaid_common/libs/services/layout-utils.svc.js
+++ src/app/_mermaid_common/libs/services/layout-utils.svc.js
@@ -13,25 +13,10 @@ angular.module('mermaid.libs').service('layoutUtils', [
       }
     };
 
-    var toggleNav = function(state, fromState) {
+    var toggleNav = function(state) {
       const toQry = 'a[data-ui-sref="' + state + '"]';
-      const fromQry = 'a[data-ui-sref="' + fromState + '"]';
       const $navItem = $(toQry);
-      const $prevNavItem = $(fromQry);
       const $parent = $navItem.closest('ul[data-smart-menu]');
-      const $navTree = $navItem.parentsUntil('ul[data-smart-menu]');
-      const $prevNavTree = $prevNavItem.parentsUntil('ul[data-smart-menu]');
-      //also refers the index of menu collapse item such as Fish Name in Reference site.
-      const navTreeLength = $navTree.length - 1;
-
-      const inElemSelection = function(elem, sel2) {
-        for (let n = sel2.length; n >= 0; n--) {
-          if (elem == sel2[n]) {
-            return true;
-          }
-        }
-        return false;
-      };
 
       _.each($parent.find('li[data-menu-collapse]'), function(collapsibleMenu) {
         if (
@@ -43,27 +28,18 @@ angular.module('mermaid.libs').service('layoutUtils', [
         }
       });
 
-      for (let i = navTreeLength; i >= 0; i--) {
-        if (
-          i === navTreeLength &&
-          $navTree[i].nodeName === 'LI' &&
-          !$($navTree[i]).hasClass('open')
-        ) {
-          $($navTree[i]).smartCollapseToggle();
-        }
-      }
+      $parent
+        .find('li.open')
+        .not($navItem)
+        .not($navItem.closest('li[data-menu-collapse]'))
+        .removeClass('open');
 
-      for (let j = 0; j < $prevNavTree.length; j++) {
-        const $elem = $($prevNavTree[j]);
-        if (
-          inElemSelection($elem[0], $navTree) === false &&
-          $elem.hasClass('open')
-        ) {
-          $elem.smartCollapseToggle();
-          $elem.removeClass('active');
-        }
-      }
       activateNavItem(state);
+
+      $navItem
+        .closest('li[data-menu-collapse]')
+        .not('.open')
+        .smartCollapseToggle();
     };
 
     return {
