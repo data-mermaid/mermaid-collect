41a137b18b9a462813ca9a6e44f9e07055742ac5
Dustin Sampson <gridcell@gmail.com>
2019-11-26 23:45:08 -0800

    rollback authservice leeway

--- src/app/auth/auth.service.js
+++ src/app/auth/auth.service.js
@@ -27,8 +27,7 @@ angular.module('app.auth').service('authService', [
 
     var mePromise = null;
     var tokenRenewalTimeout;
-    var tokenRenewLeeway = 10000; // 5 minutes
-    // var tokenRenewLeeway = 300000; // 5 minutes
+    var tokenRenewLeeway = 300000; // 5 minutes
 
     function login() {
       if (connectivity.isOnline !== true) {
