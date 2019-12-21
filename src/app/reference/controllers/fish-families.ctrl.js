4fa90b71818d77fec48c8bc061e2a51a3d3cebd3
Nick Hoang <nickhoang11@gmail.com>
2019-12-20 16:49:51 -0800

    Add filtered label for fish families reference site

--- src/app/reference/controllers/fish-families.ctrl.js
+++ src/app/reference/controllers/fish-families.ctrl.js
@@ -8,6 +8,7 @@ angular.module('app.reference').controller('FishFamiliesCtrl', [
 
     $scope.resource = null;
     $scope.tableControl = {};
+    let fishFamilyRecordsCount = 0;
 
     $scope.tableConfig = {
       id: 'fishfamiles',
@@ -39,15 +40,52 @@ angular.module('app.reference').controller('FishFamiliesCtrl', [
           display: 'Biomass Constant C',
           sortable: true
         }
-      ]
+      ],
+      toolbar: {
+        template: 'app/reference/partials/fish-families-toolbar.tpl.html',
+        clearFilters: function() {
+          $scope.tableControl.clearSearch();
+        }
+      }
+    };
+
+    const updateFishFamilyCount = function() {
+      $scope.projectObjectsTable.count().then(function(count) {
+        fishFamilyRecordsCount = count;
+      });
     };
 
     var promise = offlineservice.FishFamiliesTable();
     promise.then(function(table) {
+      $scope.projectObjectsTable = table;
+      updateFishFamilyCount();
       $scope.resource = new PaginatedOfflineTableWrapper(table, {
         searchFields: ['name']
       });
+      $scope.projectObjectsTable.$watch(
+        updateFishFamilyCount,
+        null,
+        'fishFamilyRecordsCount'
+      );
     });
+
+    $scope.tableControl.getFilteredRecordsCount = function() {
+      const tableRecordsTotal =
+        $scope.tableControl.getPaginationTable() &&
+        $scope.tableControl.getPaginationTable().total;
+
+      return `${tableRecordsTotal}/${fishFamilyRecordsCount}`;
+    };
+
+    $scope.tableControl.recordsNotFiltered = function() {
+      if (
+        $scope.tableControl.records &&
+        $scope.tableControl.records.length !== fishFamilyRecordsCount
+      ) {
+        updateFishFamilyCount();
+      }
+      return !$scope.tableControl.textboxFilterUsed();
+    };
 
     $rootScope.PageHeaderButtons = [];
   }
