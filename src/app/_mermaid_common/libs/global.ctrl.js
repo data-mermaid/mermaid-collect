aecca3eac63fb1b78632bb32617fd45d177ac718
Dustin Sampson <gridcell@gmail.com>
2020-04-24 10:03:07 -0700

    clear localstorage on logout

--- src/app/_mermaid_common/libs/global.ctrl.js
+++ src/app/_mermaid_common/libs/global.ctrl.js
@@ -70,6 +70,12 @@ angular.module('mermaid.libs').controller('GlobalCtrl', [
     $scope.logout = function() {
       var lo = function() {
         return deleteDatabases().finally(function() {
+          for (let key in window.localStorage) {
+            if (key.indexOf('mermaid.') !== -1) {
+              window.localStorage.removeItem(key);
+              n++;
+            }
+          }
           authService.logout();
         });
       };
