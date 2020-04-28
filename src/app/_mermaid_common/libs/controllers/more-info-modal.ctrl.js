284414fc6c2485e81eeec2252955bca7703b7b47
Nick Hoang <nickhoang11@gmail.com>
2020-04-28 14:27:36 -0700

    Set up data sharing more info modal

---
+++ src/app/_mermaid_common/libs/controllers/more-info-modal.ctrl.js
@@ -0,0 +1,21 @@
+angular.module('mermaid.libs').controller('MoreInfoModalCtrl', [
+  '$scope',
+  'Button',
+  '$uibModalInstance',
+  function($scope, Button, $uibModalInstance) {
+    'use strict';
+
+    let isDisabled = false;
+    let cancelBtn = new Button();
+
+    cancelBtn.name = 'Cancel';
+    cancelBtn.classes = 'btn-default';
+    cancelBtn.enabled = !isDisabled;
+    cancelBtn.click = function() {
+      $uibModalInstance.dismiss();
+    };
+
+    $scope.modalTitle = 'Data Sharing Info';
+    $scope.$modalButtons = [cancelBtn];
+  }
+]);
--- src/app/project/directives/learn-more-button.dir.js
+++ src/app/project/directives/learn-more-button.dir.js
@@ -1,10 +1,21 @@
 angular.module('app.project').directive('learnMoreButton', [
-  function() {
+  'ModalService',
+  function(ModalService) {
     'use strict';
     return {
       restrict: 'EA',
       templateUrl: 'app/project/directives/learn-more-button.tpl.html',
-      link: function(scope) {}
+      link: function(scope) {
+        scope.modalConfig = {
+          bodyTemplateUrl:
+            'app/_mermaid_common/libs/partials/more-info-table.tpl.html',
+          controller: 'MoreInfoModalCtrl'
+        };
+
+        scope.modalTrigger = function() {
+          ModalService.open(scope.modalConfig);
+        };
+      }
     };
   }
 ]);
