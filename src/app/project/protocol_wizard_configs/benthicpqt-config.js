fff48bd1f207806bf567f8f1ceb4f93ab73d24c5
Dustin Sampson <dustin@sparkgeo.com>
2022-09-25 10:39:42 -0700

    build out the photo quadrat config for validation wizard

--- src/app/project/protocol_wizard_configs/benthicpqt-config.js
+++ src/app/project/protocol_wizard_configs/benthicpqt-config.js
@@ -1,10 +1,12 @@
 angular.module('app.project').service('BenthicPQTWizardConfig', [
   '$q',
+  '$filter',
   'BenthicBaseWizardConfig',
   'ValidateSubmitService',
   'OfflineCommonTables',
   function(
     $q,
+    $filter,
     BenthicBaseWizardConfig,
     ValidateSubmitService,
     OfflineCommonTables
@@ -12,7 +14,7 @@ angular.module('app.project').service('BenthicPQTWizardConfig', [
     'use strict';
     var service = _.extend({}, BenthicBaseWizardConfig);
 
-    service.obs_benthic_lits = {
+    service.obs_benthic_photo_quadrats = {
       templateUrl:
         'app/project/protocol_wizard_configs/partials/obs_benthic_photo_quadrats.tpl.html',
       allowSkip: true,
@@ -25,7 +27,7 @@ angular.module('app.project').service('BenthicPQTWizardConfig', [
           var keys = ValidateSubmitService.getValidationKeys(
             record,
             statusFilter
-          ).obs_benthic_lits;
+          ).obs_benthic_photo_quadrats;
           return $q.resolve(keys);
         },
         benthicAttributes: function() {
@@ -35,6 +37,50 @@ angular.module('app.project').service('BenthicPQTWizardConfig', [
             return table.filter();
           });
         }
+      }
+    };
+
+    service.quadrat_number_start = {
+      templateUrl:
+        'app/project/protocol_wizard_configs/partials/quadrat_number_start.tpl.html',
+      ignoreButtonText: function(record) {
+        var val = $filter('null_blank')(
+          _.get(record.data, 'quadrat_transect.quadrat_number_start')
+        );
+        return $q.resolve('Leave Quadrat number start as ' + val);
+      }
+    };
+
+    service.quadrat_size = {
+      templateUrl:
+        'app/project/protocol_wizard_configs/partials/qt_quadrat_size.tpl.html',
+      ignoreButtonText: function(record) {
+        var val = $filter('null_blank')(
+          _.get(record.data, 'quadrat_transect.quadrat_size')
+        );
+        return $q.resolve('Leave Quadrat size as ' + val);
+      }
+    };
+
+    service.num_quadrats = {
+      templateUrl:
+        'app/project/protocol_wizard_configs/partials/num_quadrats.tpl.html',
+      ignoreButtonText: function(record) {
+        var val = $filter('null_blank')(
+          _.get(record.data, 'quadrat_transect.num_quadrats')
+        );
+        return $q.resolve('Leave Number of quadrats as ' + val);
+      }
+    };
+
+    service.num_points_per_quadrat = {
+      templateUrl:
+        'app/project/protocol_wizard_configs/partials/num_points_per_quadrat.tpl.html',
+      ignoreButtonText: function(record) {
+        var val = $filter('null_blank')(
+          _.get(record.data, 'quadrat_transect.num_points_per_quadrat')
+        );
+        return $q.resolve('Leave Number of points per quadrat as ' + val);
       }
     };
 
