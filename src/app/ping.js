f893cb6d911d629597a3c24d5b2a7424f761b5fa
Kim Fisher <kfisher@wcs.org>
2023-04-11 04:10:43 -0400

    another try at forcing update

--- src/app/ping.js
+++ src/app/ping.js
@@ -4,20 +4,8 @@ window.ping = function() {
   return $.ajax(window.appConfig.apiUrl + 'health/', {
     cache: false,
     method: 'HEAD'
-  }).then(function(data, status, jqhr) {
-    const apiVersion = jqhr.getResponseHeader('http_api_version');
-    const mermaidApiVersion = _.get(window, 'mermaid.apiVersion');
-    const hasUpdates = _.get(window, 'mermaid.hasUpdates');
-    const isDiffVersion =
-      mermaidApiVersion != null && mermaidApiVersion !== apiVersion;
-    if (mermaidApiVersion == null || isDiffVersion) {
-      _.set(window, 'mermaid.apiVersion', apiVersion);
-      console.log('setting window.mermaid.hasUpdates');
-      if (!hasUpdates) {
-        window.mermaid.hasUpdates = isDiffVersion;
-      }
-    }
-
+  }).then(function() {
+    window.mermaid.hasUpdates = true;
     return new Date().getTime() - start;
   });
 };
